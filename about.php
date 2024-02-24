4cde51747bd234624635491df08849ecde8d3d21
ompal singh <143738234+ompal570@users.noreply.github.com>
2024-02-24 13:45:14 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,83 @@
+ <!DOCTYPE html>
+ <html>
+ <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,400;1,100;1,300;1,600&display=swap');
+</style>
+
+ </head>
+ <body>
+ 
+  <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ompal</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">services</a>
+        </li>
+      
+          <li class="nav-item">
+          <a class="nav-link" href="about.php">about</a> 
+        </li> 
+            <li class="nav-item">
+          <a class="nav-link" href="#">contact</a>
+        </li>
+        
+        < 
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div class="jumbotron">
+  <h1>ompal website</h1>
+  <p>created with tha html ,css,js,bootstrap,php,mysql</p>
+</div>
+
+
+
+<section class="my-5">
+  <div class="py-5">
+    <h3 class="text-center">ABOUT US</h3>
+  </div>
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-12">
+              <img src="images/d.png " class="img-fluid">
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-12">
+             <h2 class="display-4"> i am ompal technology.</h2>
+             <p class="py-3">Use the Synonym Slider to change more (or less) of your writing. Moving the slider to the left will give you a more accurate rephrase with fewer changes; moving it to the right means you’ll get a more creative output with more changes.
+             </p>
+             <a href="adout.php" class="btn btn-success"> check more</a>
+        </div>
+
+      </div>
+</div>
+
+</section>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,276 @@
+ <!DOCTYPE html>
+ <html>
+ <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,400;1,100;1,300;1,600&display=swap');
+</style>
+
+ </head>
+ <body>
+ 
+  <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ompal</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">services</a>
+        </li>
+      
+          <li class="nav-item">
+          <a class="nav-link" href="about.php">about</a> 
+        </li> 
+            <li class="nav-item">
+          <a class="nav-link" href="#">contact</a>
+        </li>
+        
+        < 
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+
+
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/a.JPG" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>We had such a great time in LA!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/b.JPG" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/c.JPG" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+
+<section class="my-5">
+	<div class="py-5">
+		<h3 class="text-center">ABOUT US</h3>
+	</div>
+    <div class="container-fluid">
+    <div class="row">
+	    	<div class="col-lg-6 col-md-6 col-12">
+	            <img src="images/d.png " class="img-fluid">
+	    	</div>
+
+	    	<div class="col-lg-6 col-md-6 col-12">
+	           <h2 class="display-4"> i am ompal technology.</h2>
+	           <p class="py-3">Use the Synonym Slider to change more (or less) of your writing. Moving the slider to the left will give you a more accurate rephrase with fewer changes; moving it to the right means you’ll get a more creative output with more changes.
+	           </p>
+	           <a href="adout.php" class="btn btn-success"> check more</a>
+	    	</div>
+
+	    </div>
+</div>
+
+</section>
+
+		  <section class="my-5">
+			<div class="py-5">
+				<h3 class="text-center">OUR SERVICES</h3>
+			</div>
+			<div class="container-fluid">
+				 <div class="row">
+				 	<div class="col-lg-4 col-md-4 col-12">
+				 	
+				 	<div class="card" >
+ <img class="card-img-top" src="images/e.png" alt="Card image">
+		  <div class="card-body">
+		    <h4 class="card-title">beautiful nature :)</h4>
+		    <p class="card-text">Some example text.</p>
+		    <a href="#" class="btn btn-primary">See Profile</a>
+		  </div>
+		</div>
+				 </div>
+
+
+
+		 	<div class="col-lg-4 col-md-4 col-12">
+				 	
+				 	<div class="card" >
+ <img class="card-img-top" src="images/e.png" alt="Card image">
+		  <div class="card-body">
+		    <h4 class="card-title">beautiful nature :)</h4>
+		    <p class="card-text">Some example text.</p>
+		    <a href="#" class="btn btn-primary">See Profile</a>
+		  </div>
+		</div>
+				 </div>
+
+
+				 		 	<div class="col-lg-4 col-md-4 col-12">
+				 	
+				 	<div class="card" >
+ <img class="card-img-top" src="images/e.png" alt="Card image">
+		  <div class="card-body">
+		    <h4 class="card-title">beautiful nature :)</h4>
+		    <p class="card-text">Some example text.</p>
+		    <a href="#" class="btn btn-primary">See Profile</a>
+		  </div>
+		</div>
+				 </div>
+
+
+
+</div>
+</div>
+		</section>  
+  
+
+  <section class="my-5">
+			<div class="py-5">
+				<h3 class="text-center">OUR GALLERY</h3>
+			</div>
+          <div class="container-fluid">
+          	<div class="row">
+          		<div class="col-lg-4 col-md-4 col-12">
+          			<img src="images/f.jpg" class="img-fluid pd-4 my-3">
+          		</div>
+          		<div class="col-lg-4 col-md-4 col-12">
+          			<img src="images/f.jpg" class="img-fluid pd-4 my-3">
+          		</div>
+          		<div class="col-lg-4 col-md-4 col-12">
+          			<img src="images/f.jpg" class="img-fluid pd-4 my-3">
+          		</div>
+          		<div class="col-lg-4 col-md-4 col-12">
+          			<img src="images/f.jpg" class="img-fluid pd-4 my-3">
+          		</div>
+          		<div class="col-lg-4 col-md-4 col-12">
+          			<img src="images/f.jpg" class="img-fluid pd-4 my-3">
+          		</div>
+          		<div class="col-lg-4 col-md-4 col-12">
+          			<img src="images/f.jpg" class="img-fluid pd-4 my-3">
+          		</div>
+          		<div class="col-lg-4 col-md-4 col-12">
+          			<img src="images/f.jpg" class="img-fluid pd-4 my-3">
+          		</div>
+          		
+          		<div class="col-lg-4 col-md-4 col-12">
+          			<img src="images/f.jpg" class="img-fluid pd-4 my-3">
+          		</div>
+
+          		<div class="col-lg-4 col-md-4 col-12">
+          			<img src="images/f.jpg" class="img-fluid pd-4 my-3">
+          		</div>
+          		
+          	</div>
+
+          </div>
+
+	</section>		
+
+     <section class="my-5">
+      <div class="py-5">
+        <h3 class="text-center">OUR GALLERY</h3>
+      </div>
+      
+      <div class="w-50 m-auto">
+        <form action="userinfo.php" method="post">
+          <div class="from-group">
+            <label>user name</label>
+            <input type="text" name="user" class="form-control" autocomplete="
+            off" >
+          </div>
+
+
+          <div class="from-group">
+            <label>Email id</label>
+            <input type="text" name="email" class="form-control" autocomplete="
+            off" >
+          </div>
+
+
+          <div class="from-group">
+            <label>Mobile</label>
+            <input type="text" name="mobile" class="form-control" autocomplete="
+            off" >
+          </div>
+
+
+          <div class="from-group">
+            <label>comments</label>
+          <textarea class="form-control" name="comments" style="resize: none;">
+          </textarea>
+          </div>
+           <button type="submit" class="btn btn-success">Submit
+           </button>
+
+        </form>
+
+      </div>
+
+    </section> 
+
+
+
+<footer>
+  <p class="p-3 bg-dark text-center text-white">@ompaltechnicalproduction</p>
+</footer>    
+   </br>
+
+               
+
+
+
+
+   <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+ </body>
+ </html>
